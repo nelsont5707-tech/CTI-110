ab594c4252a5e49f26a1e4cabffa91290ff87027
nelsont5707-tech <nelsont5707@student.faytechcc.edu>
2025-09-21 12:36:13 -0400

    Add files via upload

---
+++ P1LAB1_NelsonTyler.py
@@ -0,0 +1,10 @@
+# Tyler Nelson
+# 09/21/2025
+# P1LAB1
+# This program prompts the user for their first and last name and displays a welcome message.
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
